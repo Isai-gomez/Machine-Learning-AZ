44ae4266f235f4d4547a3b9eb0005a4b549fb680
Isai-gomez <isaigomezhernandez@gmail.com>
2020-07-29 17:48:46 -0500

    Datos categoricos en R

--- R/script/importarLibrary.R
+++ R/script/importarLibrary.R
@@ -3,8 +3,10 @@
 dataset <- read.csv("Data.csv")
 # Tratamiento de Nas
 dataset$Age <- ifelse(is.na(dataset$Age),
-                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE))
+                      ave(dataset$Age, FUN = function(x) median(x, na.rm = TRUE))
                       ,dataset$Age)
-dataset$Salary <- ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x,na.rm = TRUE)),
+dataset$Salary <- ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) median(x,na.rm = TRUE)),
                          dataset$Salary)
+#Codificar las variables categóricas
+
   
--- python/Script/importarLibreria.py
+++ python/Script/importarLibreria.py
@@ -23,7 +23,7 @@ imputer = SimpleImputer(missing_values = np.nan, strategy = "mean")
 imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
 
-#Codificar datos categoricos
+#Codificar datos categoricos X
 from sklearn import preprocessing
 from sklearn.preprocessing import OneHotEncoder 
 from sklearn.compose import ColumnTransformer
@@ -31,3 +31,6 @@ let_X = preprocessing.LabelEncoder()
 X[:,0] = let_X.fit_transform(X[:,0])
 ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(categories='auto'),[0])],remainder='passthrough')
 X = np.array(ct.fit_transform(X), dtype=np.float)
+#Codificar datos categoricos en Y
+labelencoder_y = preprocessing.LabelEncoder()
+y = labelencoder_y.fit_transform(y)
